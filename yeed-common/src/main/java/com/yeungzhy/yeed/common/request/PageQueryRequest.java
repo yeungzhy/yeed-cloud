58e9740ce6f15f8b3952f133d2817c2c9dacbf87
yeungzhy <yeungzhy@outlook.com>
2026-08-02 19:36:07 +0800

    feat(common): 添加通用ID请求对象和分页查询请求对象

---
+++ yeed-common/src/main/java/com/yeungzhy/yeed/common/request/PageQueryRequest.java
@@ -0,0 +1,26 @@
+package com.yeungzhy.yeed.common.request;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.experimental.Accessors;
+import org.hibernate.validator.constraints.Range;
+
+@Data
+@Accessors(chain = true)
+public class PageQueryRequest {
+
+    /** 页码 */
+    @NotNull(message = "页码不能为空")
+    @Range(min = 1, max = 3000, message = "页码必须在 1-3000 之间")
+    private Integer pageNum = 1;
+
+    /** 每页条数 */
+    @NotNull(message = "每页条数不能为空")
+    @Range(min = 1, max = 2000, message = "每页条数必须在 1-2000 之间")
+    private Integer pageSize = 10;
+
+    // 可选：排序字段，字段需要在后端维护, 防止SQL注入
+    // private String orderField;
+    // private Boolean isAsc;
+
+}
