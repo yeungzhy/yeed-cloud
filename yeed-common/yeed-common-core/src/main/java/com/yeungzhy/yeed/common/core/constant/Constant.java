28f93aed8ac9586c7b769917545af5ab50d84a21
yeungzhy <yeungzhy@outlook.com>
2026-08-26 19:06:00 +0800

    feat(common): 添加令牌请求头常量和紧凑时间戳格式

--- yeed-common/yeed-common-core/src/main/java/com/yeungzhy/yeed/common/core/constant/Constant.java
+++ yeed-common/yeed-common-core/src/main/java/com/yeungzhy/yeed/common/core/constant/Constant.java
@@ -14,6 +14,9 @@ import java.time.format.DateTimeFormatter;
  */
 public class Constant {
 
+    /** 请求头令牌标识 */
+    public static final String TOKEN_HEADER = "Authorization";
+
     /** 日期时间格式：yyyy-MM-dd HH:mm:ss */
     public static final String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
     public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(DATE_TIME_PATTERN);
@@ -23,5 +26,8 @@ public class Constant {
     /** 时间格式：HH:mm:ss */
     public static final String TIME_PATTERN = "HH:mm:ss";
     public static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern(TIME_PATTERN);
+    /** 紧凑时间戳格式：yyyyMMddHHmmssSSS（无分隔符，用于文件名/批次号等） */
+    public static final String COMPACT_DATE_TIME_PATTERN = "yyyyMMddHHmmssSSS";
+    public static final DateTimeFormatter COMPACT_DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern(COMPACT_DATE_TIME_PATTERN);
 
 }
